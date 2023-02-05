1c4417518c2bba259220f70311756e87412ada0b
Gage Langdon <gagelangdon@gmail.com>
2023-02-04 20:08:09 -0800

    feat: add compatible card check util

--- src/pages/card.tsx
+++ src/pages/card.tsx
@@ -3,19 +3,44 @@ import { Vertical } from '../components/atoms';
 import { type Card as CardType } from '../types/Card';
 import Card from '../components/Card';
 import useCard from '../context/hooks/useCard';
+import { isCardCompatible } from '../utils/card-utils';
 
 const IndexPage = () => {
-  const { generateCard } = useCard();
-  const [card, setCard] = React.useState<CardType>();
+  //   const { generateCard } = useCard();
+  const [cardA, setCardA] = React.useState<CardType>({
+    id: '1',
+    nodes: [
+      { side: 2, section: 'b' },
+      { side: 1, section: 'a' },
+    ],
+  });
+  const [cardB, setCardB] = React.useState<CardType>({
+    id: '1',
+    nodes: [
+      { side: 0, section: 'b' },
+      { side: 3, section: 'c' },
+    ],
+  });
 
-  React.useEffect(() => {
-    const newCard = generateCard();
-    setCard(newCard);
-  }, [generateCard]);
+  //   React.useEffect(() => {
+  //     setCardA(generateCard());
+  //     setCardB(generateCard());
+  //   }, [generateCard]);
+
+  const areCardsCompatible = isCardCompatible({
+    existingCard: cardA,
+    newCard: cardB,
+    offsetFromExistingCard: {
+      x: 0,
+      y: 1,
+    },
+  });
 
   return (
     <Vertical alignItems="center" mt="25%">
-      {card && <Card {...card} />}
+      {cardA && <Card {...cardA} />}
+      {cardB && <Card {...cardB} />}
+      <div>{areCardsCompatible ? 'compatible' : 'not compatible'}</div>
     </Vertical>
   );
 };
--- src/utils/card-utils.ts
+++ src/utils/card-utils.ts
@@ -1,5 +1,70 @@
-import { type CardNode } from '../types/Card';
+import { type Card, type CardNode } from '../types/Card';
 
 export const doesNodeMatch = (nodeA: CardNode, nodeB: CardNode) => {
   return nodeA.side === nodeB.side && nodeA.section === nodeB.section;
+};
+
+// check to see if a selected card can be placed next to an existing card
+export const isCardCompatible = ({
+  existingCard,
+  newCard,
+  offsetFromExistingCard,
+}: {
+  existingCard: Card;
+  newCard: Card;
+  offsetFromExistingCard: { x: number; y: number }; // 1,0 = card just played on right of existing card. 0,1 = card just played on bottom of existing card
+}) => {
+  const top = offsetFromExistingCard.x === 0 && offsetFromExistingCard.y === -1;
+  const bottom =
+    offsetFromExistingCard.x === 0 && offsetFromExistingCard.y === 1;
+  const left =
+    offsetFromExistingCard.x === -1 && offsetFromExistingCard.y === 0;
+
+  // right is defaulted below
+  // const right =
+  //   offsetFromExistingCard.x === 1 && offsetFromExistingCard.y === 0;
+
+  const CompatibleCardNodesMap: {
+    [Key in string]: { [Key in string]: string };
+  } = {
+    top: {
+      '0a': '2a',
+      '0b': '2b',
+      '0c': '2c',
+    },
+    right: {
+      '1a': '3a', // existing | right
+      '1b': '3b',
+      '1c': '3c',
+    },
+    bottom: {
+      '2a': '0a',
+      '2b': '0b',
+      '2c': '0c',
+    },
+    left: {
+      '3a': '1a',
+      '3b': '1b',
+      '3c': '1c',
+    },
+  };
+
+  const respectiveNodeMap =
+    CompatibleCardNodesMap[
+      top ? 'top' : bottom ? 'bottom' : left ? 'left' : 'right'
+    ];
+
+  const foundCompatibleNode = existingCard.nodes.find((existingNode) => {
+    const nodeKey = `${existingNode.side}${existingNode.section}`;
+    // does the card contain an active node on the side that the new card is being placed?
+    if (!Object.keys(respectiveNodeMap).includes(nodeKey)) return false; // NO
+    const nodeThatNewCardNeeds = respectiveNodeMap[nodeKey]; // YES
+
+    // does the new card have a node that matches the node that the existing card has?
+    const nodeFoundOnNewCard = newCard.nodes.find(
+      (node) => `${node.side}${node.section}` === nodeThatNewCardNeeds
+    );
+    return Boolean(nodeFoundOnNewCard);
+  });
+  return foundCompatibleNode;
 };
