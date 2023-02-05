280f7ace345a3e44f0c2657a7a537c667b9e3b31
Ian Whitfield <whitfield.ian4321@gmail.com>
2023-02-04 22:43:03 -0800

    added cards to page

--- src/components/PlayerHand.tsx
+++ src/components/PlayerHand.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import useGameController from '../context/hooks/useGameController';
+import Card from './Card'
+import { Horizontal } from './atoms';
+
 
 const Component = () => {
+  
+  const { playersHand, onCardSelect } = useGameController();
+  const Cards = playersHand.map((card, index) => <Card onClick={() => onCardSelect(index)} card={card} />)
+  
   return (
-    <div>
-      <h1>Player Hand</h1>
-    </div>
-  );
+    <Horizontal justifyContent="center" gap="12px">
+      {Cards}
+    </Horizontal> 
+  )
 };
 
 export default Component;
