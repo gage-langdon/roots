2c22d401027924b6c562742bce04fc4d39a6de06
Tapetape <jodgers@loanpal.com>
2023-02-04 20:29:39 -0800

    wip

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,10 @@
 export const gridCellWidth = 120;
 export const gridCellHeight = 120;
+
+
+export const playingFieldSize = {
+    width: 10, 
+    height: 10
+}
+
+export const handSize = 5
--- src/context/hooks/useGameController.ts
+++ src/context/hooks/useGameController.ts
@@ -1,6 +1,8 @@
 import React from 'react';
 import { type Card } from '../../types/Card';
 import useCard from './useCard';
+import { Coord } from '../../types/Coord';
+import { playingFieldSize } from '../../constants';
 
 export type GameControllerContextType = {
   playersHand: Card[];
@@ -8,19 +10,54 @@ export type GameControllerContextType = {
 
 const useGameController = () => {
   const [playersHand, setPlayersHand] = React.useState<Array<Card>>([]);
+  const [playingField, setPlayingField] = React.useState<Array<Array<Card | null>>>([]);
+  const [timeLeft, setTimeLeft] = React.useState<String>();
+  const [gameActive, setGameActive] = React.useState<Boolean>();
+  const [gameStarted, setGameStarted] = React.useState<String>();
+  const [cardSelectedId, setCardSelected] = React.useState<Number>();
   const { generateCard } = useCard();
 
   React.useEffect(
     () => {
+      startGame()
       // do something
     },
     [
       /* any change to a var you put in this array will rerun this function */
     ]
-  );
+  ); 
+
+  const onCardSelect = (index: Number) => {
+    setCardSelected(index)
+  }
+
+  const onCardPlace = (coordinate: Coord) => {
+    //place cardSelectedId from players hand and place in coord location of playing field
+  }
+
+  const startGame = () => {
+    //start timer
+    //generate playfield
+    const emptyPlayingField = new Array(playingFieldSize.height).fill(new Array(playingFieldSize.width).fill(null));
+    setPlayingField(emptyPlayingField)
+
+    console.log({emptyPlayingField})
+    
+    //generate hand
+    const playersHand = new Array()
+
+  }
 
   return {
     playersHand,
+    playingField,
+    timeLeft,
+    gameActive,
+    cardSelectedId,
+    gameStarted,
+    onCardSelect,
+    onCardPlace,
+    startGame,
   };
 };
 
